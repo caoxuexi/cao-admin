c41c5e235875b418589d99268d7f979257fca6bd
CaoStudy <969718359@qq.com>
2023-06-22 14:56:54 +0800

    feat: 第一次修改，添加登录页

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,17 +3,21 @@ module.exports = {
   env: {
     node: true
   },
-  extends: [
-    'plugin:vue/vue3-essential',
-    '@vue/standard'
-  ],
+  extends: ['plugin:vue/vue3-essential', '@vue/standard'],
   parserOptions: {
     parser: '@babel/eslint-parser'
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    quotes: "warn", // 修改为警告
-    'space-before-function-paren': 'off'
+    quotes: 'warn', // 修改为警告
+    'space-before-function-paren': 'off',
+    // 添加组件命名忽略规则
+    'vue/multi-word-component-names': [
+      'error',
+      {
+        ignores: ['index', 'main'] //需要忽略的组件名
+      }
+    ]
   }
 }
